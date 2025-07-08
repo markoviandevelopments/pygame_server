259d56f437f6b856bc8c9c8fc4a6b6d9edf68694
Willoh Brubaker <willoh_brubaker@outlook.com>
2025-07-08 18:48:17 -0500

    m

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.setblocking(False)
 
 # Define host and port
-host = '172.20.10.4'
+host = '172.20.10.2'
 port = 12346
 
 coords = [[0, 0]]
