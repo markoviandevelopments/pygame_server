3c32ab9837c029e1657869b0d19298de7e51a66e
Willoh Brubaker <willoh_brubaker@outlook.com>
2025-07-08 19:22:30 -0500

    m

--- udp_client.py
+++ udp_client.py
@@ -1,18 +1,31 @@
 import socket
+import pickle
 
 # Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Server address
-host = '172.20.10.2'
+host = '172.20.10.1'
 port = 12347
 server_address = (host, port)
 
+
+
+class Dog():
+    def __init__(self):
+        Dog.sound = "bark"
+        Dog.breed = "German Shepard"
+        Dog.walk = "Hello Folks. I am walking!!!"
+
+
+dog = Dog()
+
+
 # String to send
-message = "Hello, UDP Server!"
+message = pickle.dumps(dog)
 
 # Send the string (encode to bytes)
-client_socket.sendto(message.encode('utf-8'), server_address)
+client_socket.sendto(message, server_address)
 print(f"Sent to {server_address}: {message}")
 
 # Close socket
--- udp_server.py
+++ udp_server.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 # Create a UDP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,13 +9,22 @@ host = '0.0.0.0'
 port = 12347
 server_socket.bind((host, port))
 
+
+class Dog():
+    def __init__(self):
+        Dog.sound = "bark"
+        Dog.breed = "German Shepard"
+        Dog.walk = "Hello Folks. I am walking!!!"
+
+
+
 print(f"UDP server listening on {host}:{port}")
 
 # Receive data
 while True:
     data, addr = server_socket.recvfrom(1024)  # Buffer size of 1024 bytes
-    received_string = data.decode('utf-8')  # Decode bytes to string
-    print(f"Received from {addr}: {received_string}")
+    dog = pickle.loads(data)  # Decode bytes to string
+    print(f"Received from {addr}: {dog.bark}")
 
 # Close socket
 server_socket.close()
