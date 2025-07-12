be8c724980fe36a7e75bfffb92082b408a3abe79
Willoh Brubaker <willohbrubaker@outlook.com>
2025-07-12 16:04:54 -0500

    m

--- short_client.py
+++ short_client.py
@@ -12,8 +12,8 @@ a=('192.168.1.126',9000)
 o=O()
 
 while 1:
-    o.number+ = 1
+    o.number += 1
     client_socket.sendto(pickle.dumps(o),a)
     d , _ = client_socket.recvfrom(99)
-    o=pickle.loads(d)
+    o = pickle.loads(d)
     print(f"{o.__dict__}")
--- short_server.py
+++ short_server.py
@@ -11,8 +11,8 @@ server_socket = socket.socket(2,2)
 server_socket.bind(('',9000))
 
 while 1:
-    d,a=server_socket.recvfrom(99)
-    o=pickle.loads(d)
-    o.number+=1
+    d,a = server_socket.recvfrom(99)
+    o = pickle.loads(d)
+    o.number += 1
     server_socket.sendto(pickle.dumps(o),a)
     print(f"{o.__dict__}")
