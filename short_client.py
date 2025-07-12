aeda97b0a04e4b9026d8c844d8dfb9aba7da4164
Willoh Brubaker <willohbrubaker@outlook.com>
2025-07-12 16:03:31 -0500

    m

--- short_client.py
+++ short_client.py
@@ -1,12 +1,19 @@
 # client.py
-import socket,pickle,sys
+import socket
+import pickle
+import threading
+
 class O:
- def __init__(self,n=0):self.number=n
-k=socket.socket(2,2)
+    def __init__(self,n=0):
+        self.number=n
+
+client_socket = socket.socket(2,2)
 a=('192.168.1.126',9000)
 o=O()
+
 while 1:
-    o.number+=1
-    k.sendto(pickle.dumps(o),a)
-    d,_=k.recvfrom(99)
+    o.number+ = 1
+    client_socket.sendto(pickle.dumps(o),a)
+    d , _ = client_socket.recvfrom(99)
     o=pickle.loads(d)
+    print(f"{o.__dict__}")
--- short_server.py
+++ short_server.py
@@ -1,12 +1,18 @@
 # server.py
-import socket,pickle
+import socket
+import pickle
+import threading
+
 class O:
- def __init__(self,n=0):self.number=n
-k=socket.socket(2,2)
-k.bind(('',9000))
+    def __init__(self,n=0):
+        self.number=n
+
+server_socket = socket.socket(2,2)
+server_socket.bind(('',9000))
+
 while 1:
-    d,a=k.recvfrom(99)
+    d,a=server_socket.recvfrom(99)
     o=pickle.loads(d)
     o.number+=1
-    k.sendto(pickle.dumps(o),a)
+    server_socket.sendto(pickle.dumps(o),a)
     print(f"{o.__dict__}")
