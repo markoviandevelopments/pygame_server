cc1c8515b33c1e9532a714b4d7e08a14bdf53abf
Willoh Brubaker <willohbrubaker@outlook.com>
2025-07-12 16:17:44 -0500

    m

--- short_server.py
+++ short_server.py
@@ -3,6 +3,7 @@ import socket
 import pickle
 import threading
 import time
+import copy
 
 class O:
     def __init__(self,n=0):
@@ -13,21 +14,25 @@ num = 0
 server_socket = socket.socket(2,2)
 server_socket.bind(('',9000))
 
+ob = O()
+
 def comm():
     global num
+    global ob
     while 1:
         d,a = server_socket.recvfrom(99)
         o = pickle.loads(d)
         o.number += 1
         server_socket.sendto(pickle.dumps(o),a)
         #print(f"{o.__dict__}")
-        num = o.number
+        ob = copy.deepcopy(o)
 
 
 def main():
     global num
+    global ob
     while 1:
-        print(num)
+        print(ob.number)
         time.sleep(1)
 
 comm_thread = threading.Thread(target=comm)
