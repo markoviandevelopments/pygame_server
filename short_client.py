89ed444a037b4a07a85cfd607b51416eddeba953
Willoh Brubaker <willoh_brubaker@outlook.com>
2025-07-12 16:35:13 -0500

    short_client threading

--- short_client.py
+++ short_client.py
@@ -2,6 +2,7 @@
 import socket
 import pickle
 import threading
+import time
 
 class O:
     def __init__(self,n=0):
@@ -11,9 +12,26 @@ client_socket = socket.socket(2,2)
 a=('192.168.1.126',9000)
 o=O()
 
-while 1:
-    o.number += 1
-    client_socket.sendto(pickle.dumps(o),a)
-    d , _ = client_socket.recvfrom(99)
-    o = pickle.loads(d)
-    print(f"{o.__dict__}")
+def comm():
+    global o
+    while 1:
+        o.number += 1
+        client_socket.sendto(pickle.dumps(o),a)
+        d , _ = client_socket.recvfrom(99)
+        o = pickle.loads(d)
+        # print(f"{o.__dict__}")
+
+def main():
+    global o
+    while 1:
+        print(o.number)
+        time.sleep(1)
+
+comm_thread = threading.Thread(target=comm)
+main_thread = threading.Thread(target=main)
+
+comm_thread.start()
+main_thread.start()
+
+comm_thread.join()
+main_thread.join()
